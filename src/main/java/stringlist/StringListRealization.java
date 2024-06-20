9ea35db0b283ac845904b5939b7d978d5636c08c
agorohov <aleksandr.gorohov@gmail.com>
2024-06-20 22:18:45 +0300

    initial branch commit

---
+++ src/main/java/stringlist/StringListRealization.java
@@ -0,0 +1,73 @@
+package stringlist;
+
+public class StringListRealization implements StringList {
+    @Override
+    public String add(String item) {
+        return "";
+    }
+
+    @Override
+    public String add(int index, String item) {
+        return "";
+    }
+
+    @Override
+    public String set(int index, String item) {
+        return "";
+    }
+
+    @Override
+    public String remove(String item) {
+        return "";
+    }
+
+    @Override
+    public String remove(int index) {
+        return "";
+    }
+
+    @Override
+    public boolean contains(String item) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public String get(int index) {
+        return "";
+    }
+
+    @Override
+    public boolean equals(StringList otherList) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public String[] toArray() {
+        return new String[0];
+    }
+}

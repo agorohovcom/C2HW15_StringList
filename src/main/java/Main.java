21cd2cef1151146009ce36da164883a7612c5da8
agorohov <aleksandr.gorohov@gmail.com>
2024-06-26 15:40:28 +0300

    реализовал расширение списка и автоматическое уменьшение

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,4 @@
-import stringlist.StringList;
-import stringlist.StringListRealization;
-
 public class Main {
     public static void main(String[] args) {
-        StringList s = new StringListRealization();
     }
 }
